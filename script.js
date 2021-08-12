36184be897d45f1fa9b008677da5de58defbaddc
Dmitry Krutkin <84284545+lun2t1k@users.noreply.github.com>
2021-08-12 18:11:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,6 @@ var accordion = (function (element) {
 
     var accordion1 = accordion();
     accordion1.init('#accordion');
+
+    var accordion2 = accordion();
+    accordion2.init('#accordion2');
